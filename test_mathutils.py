11bc3dd222441324568a5780b4f485fd37c1ffed
f1alej <alejandrovaladezmail@gmail.com>
2026-08-27 16:31:31 -0500

    Add missing test for multiply()

--- test_mathutils.py
+++ test_mathutils.py
@@ -1,4 +1,4 @@
-from mathutils import add, subtract
+from mathutils import add, subtract, multiply
 
 
 def test_add():
@@ -7,3 +7,7 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(4, 3) == 12
